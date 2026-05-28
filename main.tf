cd6fa3eabd82361faf98a7e968933653532d9227
miguelh612 <mi.villagra@duocuc.cl>
2026-05-28 16:12:39 -0400

    Añadidos algunos output

---
+++ main.tf
@@ -0,0 +1,29 @@
+# ─── S3 BUCKET ─────────────────────────────────────────────────────────────────
+resource "aws_s3_bucket" "storage" {
+  bucket = "mi-bucket-prueba2-${random_id.suffix.hex}"
+
+  tags = {
+    Name        = "bucket-prueba2"
+    Environment = "dev"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "storage" {
+  bucket = aws_s3_bucket.storage.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "storage" {
+  bucket                  = aws_s3_bucket.storage.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "random_id" "suffix" {
+  byte_length = 4
+}
