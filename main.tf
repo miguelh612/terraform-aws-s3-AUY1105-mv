7a628cb2cfcb100d501dc79137e43e620a0c8fd7
miguelh612 <mi.villagra@duocuc.cl>
2026-05-28 16:32:33 -0400

    arreglos en bucket name

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "storage" {
-  bucket = var.bucket_name
+  bucket = "mi-bucket-prueba2-${random_id.suffix.hex}"
 
   tags = {
     Name        = var.bucket_name_tag
--- variables.tf
+++ variables.tf
@@ -1,9 +1,3 @@
-variable "bucket_name" {
-  description = "Nombre del bucket S3"
-  type        = string
-  default     = "mi-bucket-prueba2-${random_id.suffix.hex}"
-}
-
 variable "bucket_name_tag" {
   description = "Valor de la etiqueta Name para el bucket S3"
   type        = string
