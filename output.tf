926cc930651530dd1031a6c1528db827f33ea2dd
miguelh612 <mi.villagra@duocuc.cl>
2026-07-02 15:58:57 -0400

    correcciones en output.tf

--- output.tf
+++ output.tf
@@ -1,11 +1,11 @@
-output "s3_bucket_name" {
-  value = aws_s3_bucket.storage.bucket
-}
+# output "s3_bucket_name" {
+#   value = aws_s3_bucket.storage.bucket
+# }
 
-output "s3_bucket_arn" {
-  value = aws_s3_bucket.storage.arn
-}
+# output "s3_bucket_arn" {
+#   value = aws_s3_bucket.storage.arn
+# }
 
-output "s3_bucket_versioning_status" {
-  value = aws_s3_bucket_versioning.storage.versioning_configuration[0].status
-}
+# output "s3_bucket_versioning_status" {
+#   value = aws_s3_bucket_versioning.storage.versioning_configuration[0].status
+# }
