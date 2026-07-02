0a6dd107482feb2e2128aec97d78a490ab894b26
miguelh612 <mi.villagra@duocuc.cl>
2026-07-02 15:55:52 -0400

    correcciones en main.tf

--- main.tf
+++ main.tf
@@ -7,21 +7,21 @@
 #   }
 # }
 
-resource "aws_s3_bucket_versioning" "storage" {
-  bucket = aws_s3_bucket.storage.id
+# resource "aws_s3_bucket_versioning" "storage" {
+#   bucket = aws_s3_bucket.storage.id
 
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
+#   versioning_configuration {
+#     status = "Enabled"
+#   }
+# }
 
-resource "aws_s3_bucket_public_access_block" "storage" {
-  bucket                  = aws_s3_bucket.storage.id
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
+# resource "aws_s3_bucket_public_access_block" "storage" {
+#   bucket                  = aws_s3_bucket.storage.id
+#   block_public_acls       = true
+#   block_public_policy     = true
+#   ignore_public_acls      = true
+#   restrict_public_buckets = true
+# }
 
 resource "random_id" "suffix" {
   byte_length = 4
